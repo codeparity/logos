b02780d39dde7ba478d7e91a59889334e961123d
Ashish aka Paw <ashish@codeparity.com>
2025-10-17 01:07:31 +0530

    small steps

--- symbol.py
+++ symbol.py
@@ -1,11 +1,22 @@
 import os
-import drawsvg as draw
+import drawsvg as dw
+import argparse
+import logging
+import sys
+
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+log = logging.getLogger(__name__)
 
 
-def main():
-    print("Hi")
+def main(size: int):
+    log.info(size)
     os.system("feh example.svg")
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-size", help="size of the logo", type=int, default=400)
+    args = parser.parse_args()
+    if args.size:
+        log.info("size is :" + str(args.size))
+    main(args.size)
