191013f1f1384428f4b5e29558aede013cc335f6
Ashish aka Paw <ashish@codeparity.com>
2025-10-17 22:19:26 +0530

    some

--- symbol.py
+++ symbol.py
@@ -8,8 +8,14 @@ logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 log = logging.getLogger(__name__)
 
 
+def make_square_logos(d: draw.Drawing):
+    line = draw.Line(0, 0, d.width, d.height, stroke="black")
+    d.append(line)
+
+
 def create_svg(size: int, filename: str):
-    d = draw.Drawing(size, size, origin="center")
+    d = draw.Drawing(size, size)
+    make_square_logos(d)
     d.save_svg(filename)
     log.info(f"Output file saved as {filename}")
 
