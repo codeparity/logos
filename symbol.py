88bba4b6f03d4f08849ec494a57e0a357b51779f
Ashish aka Paw <ashish@codeparity.com>
2025-11-16 05:46:04 +0530

    more

--- symbol.py
+++ symbol.py
@@ -1,3 +1,4 @@
+from enum import Enum
 import os
 import drawsvg as draw
 import argparse
@@ -10,6 +11,15 @@ logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 log = logging.getLogger(__name__)
 
 
+class Choice(draw.Rectangle):
+    def __init__(self, x, y, width, height, **kwargs):
+        super().__init__(x, y, width, height, **kwargs)
+
+        # do calculations here
+        self.mid_x = x + width // 2
+        self.mid_y = y + height // 2
+
+
 def test_colors(d: draw.Drawing):
     start = 0
     for i, value in enumerate(Dark):
@@ -18,14 +28,35 @@ def test_colors(d: draw.Drawing):
         d.append(draw.Line(10, y, 390, y, stroke=value, stroke_width=10))
 
 
-def make_square_logos(d: draw.Drawing):
+def test(d: draw.Drawing):
+    x_33 = d.width // 3
+    y_33 = d.height // 3
+    p = draw.Path(stroke=Dark.gray_40_pc, fill="none", stroke_width=1)
+    d.append(p.M(0, 0).Q(x_33, y_33, 400, 300))
+
+
+def make_bg_squares(d: draw.Drawing):
     x_33 = d.width // 3
     y_33 = d.height // 3
     # test_colors(d)
-    bg = draw.Rectangle(0, 0, d.width, d.height, fill=Dark.bg, stroke=None)
+    # Curve only (left)
+    bg = Choice(0, 0, d.width, d.height, fill=Dark.bg, stroke=None)
     d.append(bg)
-    line = draw.Line(0, 0, d.width, d.height, stroke=Dark.gray_91_pc, stoke_width=5)
-    d.append(line)
+    for x in range(0, d.width, x_33):
+        for y in range(0, d.height, y_33):
+            rect = draw.Rectangle(
+                x, y, x + x_33, y + y_33, stroke=Dark.fg, stroke_width=1
+            )
+            d.append(rect)
+            # now draw the middle points
+    test(d)
+
+
+def make_square_logos(d: draw.Drawing):
+    # line = draw.Line(0, 0, d.width, d.height, stroke=Dark.fg, stoke_width=5)
+    # d.append(line)
+    # test(d)
+    make_bg_squares(d)
 
 
 def create_svg(size: int, filename: str):
